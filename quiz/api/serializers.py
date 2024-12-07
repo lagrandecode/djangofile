9ec171e5e2e892ada3b2be9a544ca1a2b7285624
lagrandecode <seunogunmolufirst1@gmail.com>
2024-12-06 22:31:20 -0500

    "commit"

--- quiz/api/serializers.py
+++ quiz/api/serializers.py
@@ -19,18 +19,18 @@ class ResultSerializer(serializers.ModelSerializer):
         models = Result
         fields = '__all__'
 
-# class RegisterSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
-#     def create(self, validated_data):
-#         # Create and return a new user with the provided details
-#         user = User.objects.create_user(
-#             username=validated_data['username'],
-#             email=validated_data['email'],
-#             password=validated_data['password']
-#         )
-#         return user
+    def create(self, validated_data):
+        # Create and return a new user with the provided details
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- quiz/api/urls.py
+++ quiz/api/urls.py
@@ -1,6 +1,6 @@
 
 
-from .views import QuestionList, QuestionDetail
+from .views import QuestionList, QuestionDetail, RegisterView
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,4 +8,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',QuestionList.as_view()),
     path('<int:pk>/',QuestionDetail.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
